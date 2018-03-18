95ae9bf083285efb4f1412cd40c03cbb95ad0aea
АртурГудиев <@DESKTOP-SKG4SS4>
2018-03-18 17:31:10 +0300

    First commit

--- DslPackage/GeneratedCode/Package.cs
+++ DslPackage/GeneratedCode/Package.cs
@@ -76,6 +76,14 @@ namespace Company.FamilyTree
 					"@NationalityToolToolboxBitmap;Company.FamilyTree.Dsl.dll", 
 					0xff00ff,
 					Index = 5)]
+	[VSShell::ProvideStaticToolboxItem("Company.FamilyTree.FamilyTreeToolboxTab",
+					"@ElementTool1ToolboxItem;Company.FamilyTree.Dsl.dll", 
+					"Company.FamilyTree.ElementTool1ToolboxItem", 
+					"CF_TOOLBOXITEMCONTAINER,CF_TOOLBOXITEMCONTAINER_HASH,CF_TOOLBOXITEMCONTAINER_CONTENTS", 
+					"ElementTool1", 
+					"@ElementTool1ToolboxBitmap;Company.FamilyTree.Dsl.dll", 
+					0xff00ff,
+					Index = 6)]
 	[VSShell::ProvideEditorFactory(typeof(FamilyTreeEditorFactory), 103, TrustLevel = VSShellInterop::__VSEDITORTRUSTLEVEL.ETL_AlwaysTrusted)]
 	[VSShell::ProvideEditorExtension(typeof(FamilyTreeEditorFactory), "." + Constants.DesignerFileExtension, 50)]
 	[VSShell::ProvideEditorLogicalView(typeof(FamilyTreeEditorFactory), "{7651A702-06E5-11D1-8EBD-00A0C90F26EA}")] // Designer logical view GUID i.e. VSConstants.LOGVIEWID_Designer
